8493512ac92424a85a2922dc7564c187860d1830
Chris Perl <chris.perl@gmail.com>
2011-07-11 15:55:31 -0400

    Updating to hopefully make hexdump work with both python 2 and 3

--- python_startup.py
+++ python_startup.py
@@ -9,13 +9,15 @@ def hexdump(data):
     while offset < len(data):
         col1  = data[offset:offset+8]
         col2  = data[offset+8:offset+16]
-        col1  = [ ord(x) for x in col1 ]
-        col2  = [ ord(x) for x in col2 ]
+        # hack for python 3
+        if isinstance(data, str):
+            col1  = [ ord(x) for x in col1 ]
+            col2  = [ ord(x) for x in col2 ]
         ascii = [ chr(x) if 31 < x < 127 else '.' for x in col1 + col2 ]
         fmt = "{0:08x}  {1:23}  {2:23}  |{3:16}|"
         s =  fmt.format(offset, 
                         ' '.join([ "{0:02x}".format(x) for x in col1]),
                         ' '.join([ "{0:02x}".format(x) for x in col2]),
                         ''.join(ascii))
-        print s
+        print(s)
         offset = offset + 16
