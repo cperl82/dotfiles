3b4b8d20bd6b5ed300347c6ada3cacfd9b22f34c
Chris Perl <chris.perl@gmail.com>
2014-03-31 21:51:41 -0400

    add init.el

---
+++ init.el
@@ -0,0 +1,138 @@
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(require 'el-get)
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user-receipes")
+(setq el-get-verbose t)
+
+(setq my-packages
+      '(ack-and-a-half chumpy-windows color-theme
+      color-theme-almost-monokai color-theme-black-purple
+      color-theme-chocolate-rain color-theme-emacs-revert-theme
+      color-them e-inkpot color-theme-ir-black
+      color-theme-mac-classic color-theme-solarized
+      color-theme-tomorrow color-theme-zenburn el-get escreen evil
+      evil-leader tuareg-mode undo-tree xoria256-emacs))
+
+(el-get 'sync my-packages)
+
+; 2014-03-27: Turn off the menu bar
+(menu-bar-mode -1)
+
+; 2014-03-27: Always show matching parents
+(setq show-paren-delay 0)
+(show-paren-mode)
+
+; 2014-03-27: Quick window changing
+(global-set-key "\M-h" 'windmove-left)
+(global-set-key "\M-j" 'windmove-down)
+(global-set-key "\M-k" 'windmove-up)
+(global-set-key "\M-l" 'windmove-right)
+
+; 2014-03-27: Do not want backup files
+(setq make-backup-files nil)
+
+; 2014-03-27: Evil
+(setq-default evil-symbol-word-search t)
+(require 'evil)
+(evil-mode 1)
+(evil-set-initial-state 'ibuffer-mode 'normal)
+
+; 2014-03-27: Evil Leader
+(require 'evil-leader)
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+(evil-leader/set-key
+  "f" 'find-file
+  "b" 'ibuffer
+  "s" 'split-window-vertically
+  "v" 'split-window-horizontally
+  "k" 'kill-buffer
+  "o" 'delete-other-windows
+  "x" 'delete-window
+  "e" 'escreen-get-active-screen-numbers-with-emphasis)
+
+; 2014-03-28: Functions to support selecting something in Visual mode
+; and then automatically start searching for it by pressing "/" or "?"
+(evil-define-operator cp-evil-search (beg end forward)
+  (let* ((search-string (buffer-substring beg end))
+	 (quoted-string (regexp-quote search-string)))
+    (setq isearch-forward forward)
+    (evil-search quoted-string forward t)))
+
+(evil-define-operator cp-evil-search-forward (beg end type)
+  (cp-evil-search beg end t))
+
+(evil-define-operator cp-evil-search-backward (beg end type)
+  (cp-evil-search beg end nil))
+
+(define-key evil-visual-state-map "/" `cp-evil-search-forward)
+(define-key evil-visual-state-map "?" `cp-evil-search-backward)
+
+; 2014-03-27: ack-and-a-half: https://github.com/jhelwig/ack-and-a-half
+(require 'ack-and-a-half)
+
+; 2014-03-29: org-mode
+(require 'org)
+
+; 2014-03-29: ido
+(require 'ido)
+(ido-mode t)
+
+; 2014-03-30: tuareg mode
+(require 'tuareg)
+
+;;; escreen
+(require 'escreen)
+(escreen-install)
+
+;; add C-\ l to list screens with emphase for current one
+(defun escreen-get-active-screen-numbers-with-emphasis ()
+  "what the name says"
+  (interactive)
+  (let ((escreens (escreen-get-active-screen-numbers))
+	(emphased ""))
+
+    (dolist (s escreens)
+      (setq emphased
+	    (concat emphased (if (= escreen-current-screen-number s)
+				 (concat "(*" (number-to-string s) ")")
+			       (number-to-string s))
+		    " ")))
+    (message "escreen: active screens: %s" emphased)))
+
+(global-set-key (kbd "C-\\ l") 'escreen-get-active-screen-numbers-with-emphasis)
+
+(defun dim:escreen-goto-last-screen ()
+  (interactive)
+  (escreen-goto-last-screen)
+  (escreen-get-active-screen-numbers-with-emphasis))
+
+(defun dim:escreen-goto-prev-screen (&optional n)
+  (interactive "p")
+  (escreen-goto-prev-screen n)
+  (escreen-get-active-screen-numbers-with-emphasis))
+
+(defun dim:escreen-goto-next-screen (&optional n)
+  (interactive "p")
+  (escreen-goto-next-screen n)
+  (escreen-get-active-screen-numbers-with-emphasis))
+
+(define-key escreen-map escreen-prefix-char 'dim:escreen-goto-last-screen)
+
+(global-set-key '[M-S-right] (quote dim:escreen-goto-next-screen))
+(global-set-key '[M-S-left] (quote dim:escreen-goto-prev-screen))
+
+(global-set-key (kbd "M-[") 'dim:escreen-goto-prev-screen)
+(global-set-key (kbd "M-]") 'dim:escreen-goto-next-screen)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("4031c1ea0bb235b75a048bd92f3bf3aa984c9f7cc5b408f00f62ed99a6eecc09" "8b30636c9a903a9fa38c7dcf779da0724a37959967b6e4c714fdc3b3fe0b8653" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
