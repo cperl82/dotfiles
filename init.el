2c3360153a44e5590f555945dc7546e4a5fcc95a
Chris Perl <chris.perl@gmail.com>
2014-04-09 10:33:26 -0400

    wip

--- init.el
+++ init.el
@@ -242,3 +242,21 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 ; 2014-04-08: local emacs overrides
 (let ((local "~/.emacs.local"))
   (when (file-exists-p local) (load-file local)))
+
+; 2014-04-09: trying to get some stuff done with code folding
+; http://www.emacswiki.org/emacs/HideShow
+(defun toggle-selective-display (column)
+      (interactive "P")
+      (set-selective-display
+       (or column
+           (unless selective-display
+             (1+ (current-column))))))
+
+(defun toggle-hiding (column)
+      (interactive "P")
+      (if hs-minor-mode
+          (if (condition-case nil
+                  (hs-toggle-hiding)
+                (error t))
+              (hs-show-all))
+        (toggle-selective-display column)))
