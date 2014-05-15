ec0907750c801c14d0534d3fa9e9ea3952fa76ee
Chris Perl <chris.perl@gmail.com>
2014-05-14 20:45:55 -0400

    Turn off dired-perm-write-face.

--- init.el
+++ init.el
@@ -500,7 +500,7 @@ buffers whose visited file has disappeared and refreshes dired buffers."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(dired-perm-write ((t nil))))
 
 (setq c-default-style "linux")
 
