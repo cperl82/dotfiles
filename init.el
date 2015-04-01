715d9cfc38a6e9f528e155c746896c307bcc1d3a
Chris Perl <chris.perl@gmail.com>
2015-04-01 10:35:11 -0400

    Normalize agenda mappings inside and outside of agenda buffer

--- init.el
+++ init.el
@@ -447,7 +447,8 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 (evil-define-key 'emacs org-agenda-mode-map (kbd "h") 'left-char)
 (evil-define-key 'emacs org-agenda-mode-map (kbd "l") 'right-char)
 (evil-define-key 'emacs org-agenda-mode-map (kbd " ") 'org-agenda-cycle-show)
-(evil-define-key 'emacs org-agenda-mode-map (kbd "a") 'org-agenda)
+(evil-define-key 'emacs org-agenda-mode-map (kbd "C-c a") 'org-agenda)
+(evil-define-key 'emacs org-agenda-mode-map (kbd "C-c c") 'org-capture)
 (evil-define-key 'emacs org-agenda-mode-map (kbd "C-h") 'evil-window-left)
 (evil-define-key 'emacs org-agenda-mode-map (kbd "C-l") 'evil-window-right)
 
