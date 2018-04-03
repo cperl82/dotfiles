e3aac5e08a6cf6bb6ef2260660ba0f7a97d2a7d5
Chris Perl <chris.perl@gmail.com>
2018-04-03 19:51:20 -0400

    moar better

--- i3/bin/quickswitch.sh
+++ i3/bin/quickswitch.sh
@@ -9,107 +9,114 @@ function usage {
 
 function scratchpad-window-query {
     cat <<-'EOF'
-	  ..
-	| select(.name? == "__i3_scratch")
-	| .floating_nodes
-	| map(.nodes | .[0])
-	| [ (map(select(.orientation != "none")) | map({id: .id, windows: .nodes | map(.name)}))
-	  , (map(select(.orientation == "none")) | map({id: .id, windows: [.name]}            ))
-	  ]
-	| add
-	| map([(.id | tostring), (.windows | join(", "))])
-	| map(join(" "))
-	| .[]
+        .nodes
+        | map(select(.name? == "__i3"))
+        | .[0]
+        | .nodes
+        | .[0]
+        | .nodes
+        | map(select(.name? == "__i3_scratch"))
+        | .[0]
+        | .floating_nodes
+        | map(.nodes)
+        | add
+        | map({ id: (.id | tostring)
+              , windows: [.. | select(.nodes? == [] and .floating_nodes? == [])] | map(.name) | join(", ")})
+        | map(join(" "))
+        | .[]
 	EOF
 }
 
 function non-scratchpad-window-query {
     cat <<-'EOF'
-	  ..
-	| select(.type? == "output")
-	| select(.name? != "__i3")
-	| .nodes
-	| map(select(.type? != "dockarea"))
-	| [ .. | select(.nodes? == [] and .floating_nodes == [] and .focused == false) ]
-	| map([(.id | tostring), .name])
-	| map(join(" "))
-	| .[]
+          ..
+        | select(.type? == "output")
+        | select(.name? != "__i3")
+        | .nodes
+        | map(select(.type? != "dockarea"))
+        | [ .. | select(.nodes? == [] and .floating_nodes == [] and .focused == false) ]
+        | map([(.id | tostring), .name])
+        | map(join(" "))
+        | .[]
 	EOF
 }
 
 function workspace-query {
     cat <<-'EOF'
-	[ .. | select(.type? == "workspace") ]
-	| map(select(.name != "__i3_scratch"))
-	| map({id: (.id | tostring), name: .name})
-	| map(join(" "))
-	| .[]
+        [ .. | select(.type? == "workspace") ]
+        | map(select(.name != "__i3_scratch"))
+        | map({id: (.id | tostring), name: .name})
+        | map(join(" "))
+        | .[]
 	EOF
 }
 
-function restore-from-scratchpad-cmd {
+function subcmd--restore-from-scratchpad {
     local tree
     local q
 
     tree=$(i3-msg -t get_tree)
     q=$(scratchpad-window-query)
 
-    jq -r "${q}" <<< "${tree}"							\
-	| sort -k 2								\
-	| fzf --with-nth=2.. --border --multi --no-sort				\
-	| awk '{print $1}'							\
-	| xargs -n1 -I{} i3-msg -t command "[con_id={}] scratchpad show"
+    jq -r "${q}" <<< "${tree}"                                                  \
+        | sort -k 2                                                             \
+        | fzf --with-nth=2.. --border --multi --no-sort                         \
+        | awk '{print $1}'                                                      \
+        | xargs -n1 -I{} i3-msg -t command "[con_id={}] scratchpad show"
 }
 
-function jump-to-window-cmd {
+function subcmd--jump-to-window {
     local tree
     local q
 
     tree=$(i3-msg -t get_tree)
     q=$(non-scratchpad-window-query)
 
-    jq -r "${q}" <<< "${tree}"						\
-	| fzf --with-nth=2.. --border					\
-	| awk '{print $1}'						\
-	| xargs -n1 -I{} i3-msg -t command "[con_id={}] focus"
+    jq -r "${q}" <<< "${tree}"                                  \
+        | fzf --with-nth=2.. --border                           \
+        | awk '{print $1}'                                      \
+        | xargs -n1 -I{} i3-msg -t command "[con_id={}] focus"
 }
 
-function jump-to-workspace-cmd {
+function subcmd--jump-to-workspace {
     local tree
     local q
 
     tree=$(i3-msg -t get_tree)
     q=$(workspace-query)
 
-    jq -r "${q}" <<< "${tree}"						\
-	| fzf --with-nth=2.. --border					\
-	| awk '{print $1}'						\
-	| xargs -n1 -I{} i3-msg -t command "[con_id={}] focus"
+    jq -r "${q}" <<< "${tree}"                                  \
+        | fzf --with-nth=2.. --border                           \
+        | awk '{print $1}'                                      \
+        | xargs -n1 -I{} i3-msg -t command "[con_id={}] focus"
 
 }
 
 function main {
-    local cmds
+    local subcmd_prefix="subcmd"
+    local completions
+    local subcmd
 
     while [[ ${#} -gt 0 ]]
     do
-	case "${1}" in
-	    -*)
-		usage
-		exit 1
-		;;
-	    *)
-		break
-		;;
-	esac
+        case "${1}" in
+            -*)
+                usage
+                exit 1
+                ;;
+            *)
+                break
+                ;;
+        esac
     done
 
-    cmds=( $(compgen -A function -- "${1}" | grep -- "-cmd$") )
-    shift
+    completions=$(declare -F | awk '$2 ~ /^-f$/ {print $NF}' | sed -ne "s/^${subcmd_prefix}--//p")
+    subcmd=($(compgen -W "${completions}" -- "${1}"))
 
-    if [[ ${#cmds[@]} -eq 1 ]]
+    if [[ ${#subcmd[@]} -eq 1 ]]
     then
-        ${cmds[0]} "$@"
+        shift
+        "${subcmd_prefix}--${subcmd[0]}" "$@"
     else
         usage
         exit 1
