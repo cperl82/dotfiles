fa647cda976d414268757592a8c6851d6421c513
Chris Perl <chris.perl@gmail.com>
2018-09-23 09:12:38 -0400

    Spacing

--- i3/bin/generate-config.sh
+++ i3/bin/generate-config.sh
@@ -2,10 +2,10 @@
 
 main ()
 {
-	cat "${HOME}/.config/i3/config.base"	\
-	    "${HOME}/.config/i3/config.local"	\
-	    2>/dev/null				\
-	    > "${HOME}/.config/i3/config"
+    cat "${HOME}/.config/i3/config.base"	\
+	"${HOME}/.config/i3/config.local"	\
+	2>/dev/null				\
+	> "${HOME}/.config/i3/config"
 }
 
 main
