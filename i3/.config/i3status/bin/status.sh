1a96c5fcb66354066fd4c8fa8d2fd2fc7d3ae54c
Chris Perl <chris.perl@gmail.com>
2019-10-21 23:10:52 -0400

    Rework some parts of status.sh

--- i3/.config/i3status/bin/status.sh
+++ i3/.config/i3status/bin/status.sh
@@ -1,35 +1,52 @@
 #!/bin/bash
 
-function i3bar_prolog {
+i3bar_pre () {
     printf "{\"version\":1}\n"
     printf "[\n"
 }
 
-function main
-{
-    local first=1
+insert_json_at_position () {
+    local array="${1}"
+    local position="${2}"
+    local json="${3}"
+    local program=""
+
+    read -d '' -r program <<-'EOF'
+	map(select(.full_text != "")) | .[0:$idx] + [$json] + .[$idx:]
+	EOF
+
+    jq -rc --argjson json "${json}" --argjson idx "${position}" "${program}" <<< "${array}"
+}
+
+main () {
     local dir=""
+    local plugin_dir=""
     local output=""
+    local template=""
+    local program=""
 
     dir=$(dirname "${0}")
+    plugin_dir="${dir}/../plugin.d"
 
-    i3bar_prolog
+    i3bar_pre
     while read -r json
     do
-	for plugin in $(find "${dir}/plugin.d" -type f -print 2>/dev/null)
+	while read -r -d '' plugin
 	do
 	    test -x "${plugin}" || continue
 	    read -r type position output < <("${plugin}")
 	    case "${type}" in
 		 PLUGIN_OUTPUT_TEXT)
-		     read -r -d '' output <<-EOF
-			{ "name":      "${plugin}"
-			, "instance":  "0"
-			, "color":     "#FFFFFF"
-			, "markup":    "none"
-			, "full_text": "${output}"
-			}
+		     read -d '' -r template <<-'EOF'
+			{"name": "", "instance":0, "color":"#FFFFFF", "markup":"none", "full_text":""}
 			EOF
+		     read -d '' -r program <<-'EOF'
+			.name=$name | .full_text=$full
+			EOF
+		     output=$(jq -rc					\
+				 --arg name "$(basename "${plugin}")"	\
+				 --arg full "${output}"			\
+				 "${program}" <<< "${template}")
 		 ;;
 
 		 PLUGIN_OUTPUT_JSON)
@@ -45,13 +62,8 @@ function main
 
 	    # Insert the json from the plugin into the appropriate
 	    # spot in the array that will be consumed by i3bar
-	    json=$(jq -rc								\
-		      --argjson output "${output}"					\
-		      --arg     idx "${position}"					\
-		      'map(select(.full_text != ""))
-		       | .[0:($idx | tonumber)] + [$output] + .[($idx | tonumber):]'	\
-		      <<< "${json}")
-	done
+	    json=$(insert_json_at_position "${json}" "${position}" "${output}")
+	done < <(find "${plugin_dir}" -type f -print0 2>/dev/null)
 
 	# Send the output, augmented with plugin output, to i3bar
 	printf "%s,\n" "${json}"
