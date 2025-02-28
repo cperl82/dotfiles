92397fdfdd32ed08f4f2c5e1080304fcbba231ce
Chris Perl <chris.perl@gmail.com>
2025-02-27 22:18:43 -0500

    Delete empty blocks in agenda.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1575,6 +1575,40 @@ The key is the todo keyword and the value is its relative position in the list."
          (all-forms (append agenda-forms tags-todo-forms)))
     (org-agenda-run-series "" `(,all-forms ((org-agenda-buffer-name "*Org Agenda*"))))))
 
+;; 2025-02-22 cperl: Snarfed from:
+;; https://www.reddit.com/r/emacs/comments/jjrk2o/comment/gaeh3st/
+(defun cp/org-agenda-delete-empty-blocks ()
+  "Remove empty agenda blocks.
+  A block is identified as empty if there are fewer than 2 non-empty lines in
+  the block (excluding the line with `org-agenda-block-separator' characters)."
+  (when org-agenda-compact-blocks
+    (user-error "Cannot delete empty compact blocks"))
+  (setq buffer-read-only nil)
+  (save-excursion
+    (goto-char (point-min))
+    (let* ((blank-line-re "^\\s-*$")
+           (content-line-count (if (looking-at-p blank-line-re) 0 1))
+           (start-pos (point))
+           (block-re (format "%c\\{10,\\}" org-agenda-block-separator)))
+      (while (and (not (eobp)) (forward-line))
+        (cond
+         ((looking-at-p block-re)
+          (when (< content-line-count 2)
+            (delete-region start-pos (1+ (point-at-bol))))
+          (setq start-pos (point))
+          (forward-line)
+          (setq content-line-count (if (looking-at-p blank-line-re) 0 1)))
+         ((not (looking-at-p blank-line-re))
+          (setq content-line-count (1+ content-line-count)))))
+      (when (< content-line-count 2)
+        (delete-region start-pos (point-max)))
+      (goto-char (point-min))
+      ;; The above strategy can leave a separator line at the beginning
+      ;; of the buffer.
+      (when (looking-at-p block-re)
+        (delete-region (point) (1+ (point-at-eol))))))
+  (setq buffer-read-only t))
+
 (use-package org
   :defer t
   :general
@@ -1723,6 +1757,7 @@ The key is the todo keyword and the value is its relative position in the list."
     (setq org-agenda-custom-commands
           '(("." "Today's agenda with next actions tagged by context"
              cp/org-agenda-generate-and-run-forms "")))
+    (add-hook 'org-agenda-finalize-hook #'cp/org-agenda-delete-empty-blocks)
     (setq org-tags-column -85)
     (setq org-log-into-drawer t)
     (setq org-refile-use-cache t)
