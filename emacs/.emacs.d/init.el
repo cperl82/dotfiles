183d091b80c43388e1f9a318bc59a901b84e36ee
Chris Perl <chris.perl@gmail.com>
2026-02-22 14:41:39 -0500

    Attempt to reapply our customizations whenever zenburn is enabled

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1748,51 +1748,54 @@ to return a list"
   :config
   (progn
     (load-theme 'zenburn :no-confirm)
-    (zenburn-with-color-variables
-      (custom-theme-set-faces
-       'zenburn
-       `(diff-added
-         ((t (:foreground ,zenburn-green :weight bold))))
-       `(diff-removed
-         ((t (:foreground ,zenburn-red))))
-       `(linum
-         ((t (:foreground ,zenburn-green-1 :background ,zenburn-bg))))
-       '(dired-perm-write
-         ((t nil)))
-       '(hl-line
-         ((t (:background "#4F4F4F"))))
-       `(isearch
-         ((t (:inherit nil :foreground "white" :background ,zenburn-red-2))))
-       `(lazy-highlight
-         ((t (:inherit nil :foreground "white" :background ,zenburn-blue-1))))
-       `(match
-         ((t (:inherit nil :foreground "white" :background ,zenburn-yellow-2))))
-       `(info-node
-         ((t (:foreground ,zenburn-red-3))))
-       '(ivy-cursor
-         ((t (:foreground "#000000" :background "#d6d6d6"))))
-       `(ivy-current-match
-         ((t (:underline nil))))
-       '(ivy-minibuffer-match-face-1
-         ((t (:underline nil))))
-       `(ivy-minibuffer-match-face-2
-         ((t (:foreground ,zenburn-red-2))))
-       `(ivy-minibuffer-match-face-3
-         ((t (:foreground ,zenburn-blue-1))))
-       `(ivy-minibuffer-match-face-4
-         ((t (:foreground ,zenburn-yellow-2))))
-       `(ivy-virtual
-         ((t (:inherit font-lock-type-face))))
-       '(swiper-line-face
-         ((t (:underline nil :background "#4F4F4F"))))
-       '(swiper-match-face-1
-         ((t (:underline nil))))
-       `(swiper-match-face-2
-         ((t (:inherit nil :foreground "white" :background ,zenburn-red-2))))
-       `(swiper-match-face-3
-         ((t (:inherit nil :foreground "white" :background ,zenburn-blue-1))))
-       `(swiper-match-face-4
-         ((t (:inherit nil :foreground "white" :background ,zenburn-yellow-2))))))))
+    (defun cp/zenburn-theme-customize (theme)
+      (when (eq theme 'zenburn)
+        (zenburn-with-color-variables
+          (custom-theme-set-faces
+           'zenburn
+           `(diff-added
+             ((t (:foreground ,zenburn-green :weight bold))))
+           `(diff-removed
+             ((t (:foreground ,zenburn-red))))
+           `(linum
+             ((t (:foreground ,zenburn-green-1 :background ,zenburn-bg))))
+           '(dired-perm-write
+             ((t nil)))
+           '(hl-line
+             ((t (:background "#4F4F4F"))))
+           `(isearch
+             ((t (:inherit nil :foreground "white" :background ,zenburn-red-2))))
+           `(lazy-highlight
+             ((t (:inherit nil :foreground "white" :background ,zenburn-blue-1))))
+           `(match
+             ((t (:inherit nil :foreground "white" :background ,zenburn-yellow-2))))
+           `(info-node
+             ((t (:foreground ,zenburn-red-3))))
+           '(ivy-cursor
+             ((t (:foreground "#000000" :background "#d6d6d6"))))
+           `(ivy-current-match
+             ((t (:underline nil))))
+           '(ivy-minibuffer-match-face-1
+             ((t (:underline nil))))
+           `(ivy-minibuffer-match-face-2
+             ((t (:foreground ,zenburn-red-2))))
+           `(ivy-minibuffer-match-face-3
+             ((t (:foreground ,zenburn-blue-1))))
+           `(ivy-minibuffer-match-face-4
+             ((t (:foreground ,zenburn-yellow-2))))
+           `(ivy-virtual
+             ((t (:inherit font-lock-type-face))))
+           '(swiper-line-face
+             ((t (:underline nil :background "#4F4F4F"))))
+           '(swiper-match-face-1
+             ((t (:underline nil))))
+           `(swiper-match-face-2
+             ((t (:inherit nil :foreground "white" :background ,zenburn-red-2))))
+           `(swiper-match-face-3
+             ((t (:inherit nil :foreground "white" :background ,zenburn-blue-1))))
+           `(swiper-match-face-4
+             ((t (:inherit nil :foreground "white" :background ,zenburn-yellow-2))))))))
+    (add-hook 'enable-theme-functions #'cp/zenburn-theme-customize)))
 
 (enable-theme 'zenburn)
 
