f4d3cc93f49c0aaf6d8f6f47b47d27db1afc58b6
Chris Perl <chris.perl@gmail.com>
2026-06-02 22:49:56 -0400

    Update evil-collection.

--- emacs/.emacs.d/straight/versions/default.el
+++ emacs/.emacs.d/straight/versions/default.el
@@ -19,7 +19,7 @@
  ("embark" . "a4847042ce53823624d115eebfd77eb3b4477d1b")
  ("epl" . "78ab7a85c08222cd15582a298a364774e3282ce6")
  ("evil" . "b06f644bdb5b06c6ac46c11b0259f15ac9ffd5da")
- ("evil-collection" . "4748cfb78488fce96812130e0b53aae6d042ca6d")
+ ("evil-collection" . "673b38022b8bb36185b6d54aff3f444335a032fb")
  ("evil-smartparens" . "026d4a3cfce415a4dfae1457f871b385386e61d3")
  ("evil-surround" . "da05c60b0621cf33161bb4335153f75ff5c29d91")
  ("f.el" . "931b6d0667fe03e7bf1c6c282d6d8d7006143c52")
