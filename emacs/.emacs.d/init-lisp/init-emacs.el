08dcf61339712659bb31131525b486505cd6d8b8
Chris Perl <chris.perl@gmail.com>
2026-06-26 10:08:14 -0400

    Add back `confirm-kill-emacs'
    Turns out that was doing something.

--- emacs/.emacs.d/init-lisp/init-emacs.el
+++ emacs/.emacs.d/init-lisp/init-emacs.el
@@ -1,8 +1,9 @@
 (use-package emacs
   :demand t
   :custom
-  (ad-redefinition-action 'accept)
+  (ad-redefinition-action #'accept)
   (column-number-mode t)
+  (confirm-kill-emacs #'yes-or-no-p)
   (create-lockfiles nil)
   (enable-recursive-minibuffers t)
   (indent-tabs-mode nil)
@@ -16,10 +17,10 @@
   (when (> emacs-major-version 28)
     (setq native-comp-async-report-warnings-errors nil))
   (put 'narrow-to-region 'disabled nil)
-  (global-auto-revert-mode)
   (menu-bar-mode -1)
   (tool-bar-mode -1)
   (scroll-bar-mode -1)
+  (global-auto-revert-mode)
   :hook
   ((prog-mode . turn-on-auto-fill)))
 (provide 'init-emacs)
