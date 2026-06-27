1ebbdb02b28c3b1f6919f93da2bf6af9d93102d4
Chris Perl <chris.perl@gmail.com>
2026-06-27 08:17:31 -0400

    Use condition-case instead of ignore-errors

--- emacs/.emacs.d/init-lisp/init-local-overrides.el
+++ emacs/.emacs.d/init-lisp/init-local-overrides.el
@@ -1,4 +1,4 @@
-(ignore-errors
-  (load-file
-   (expand-file-name "~/.emacs.local")))
+(condition-case nil
+    (load-file (expand-file-name "~/.emacs.local"))
+  (file-error nil))
 (provide 'init-local-overrides)
