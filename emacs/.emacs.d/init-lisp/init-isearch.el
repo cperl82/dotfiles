72a773706a684545e473c3b585a5840cecfe555d
Chris Perl <chris.perl@gmail.com>
2026-07-10 23:01:51 -0400

    tangle config

--- emacs/.emacs.d/init-lisp/init-isearch.el
+++ emacs/.emacs.d/init-lisp/init-isearch.el
@@ -7,5 +7,22 @@
   (search-whitespace-regexp (rx (minimal-match (0+ any))))
   :bind
   (:map isearch-mode-map
-   ("C-o" . isearch-occur)
-   ("C-d" . isearch-forward-thing-at-point)))
+        ("C-o" . isearch-occur)
+        ("C-l" . consult-line)))
+
+(use-package isearch-mb
+  :straight t
+  :demand t
+  :bind
+  (:map isearch-mb-minibuffer-map
+        ("C-o" . loccur-isearch)
+        ("C-l" . consult-line))
+  :config
+  (add-to-list 'isearch-mb--with-buffer #'loccur-isearch)
+  (add-to-list 'isearch-mb--with-buffer #'consult-isearch-history)
+  (add-to-list 'isearch-mb--after-exit  #'consult-line)
+  (isearch-mb-mode))
+
+(use-package loccur
+  :straight t
+  :defer t)
